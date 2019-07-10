1d0f0d392b57a7f8077c856c4d9c63f12bd217b9
JankhJankh <50971975+JankhJankh@users.noreply.github.com>
2019-07-10 13:15:51 +1000

    Update main.py
    Stopping the website from caching images

--- Exercises/main.py
+++ Exercises/main.py
@@ -218,6 +218,12 @@ def upload_file():
 			images[image].save(os.path.join('static/kmeans/', file_name))
 			return redirect('/kmeans')
 
+@app.after_request
+def set_response_headers(response):
+	response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+	response.headers['Pragma'] = 'no-cache'
+	response.headers['Expires'] = '0'
+	return response
 
 if __name__ == "__main__":
     app.run()
